b699507e5e6451bd04a1181dc20e93d57586426d
anitaabramowich <aniabramowich2003@gmail.com>
2024-03-21 22:14:49 -0300

    frontend con React, funcionalidades y estilo de login/sign up

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import LoginSignup from "./Components/LoginSignup/LoginSignup";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        {/*<img src={logo} className="App-logo" alt="logo" />*/}
+        <LoginSignup />
+      </header>
+    </div>
+  );
+}
+
+export default App;
