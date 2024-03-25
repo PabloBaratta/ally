6cc5488884c992b372446c54757082e7df005cb2
PabloBaratta <pbaratta2004@gmail.com>
2024-03-25 18:35:11 -0300

    Spring - init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
 plugins {
     id("java")
+    id("war")
+    id("org.springframework.boot") version("3.2.4")
+    id("io.spring.dependency-management") version("1.1.4")
 }
 
 group = "org.example"
@@ -15,6 +18,9 @@ dependencies {
     implementation("org.hibernate:hibernate-core:5.6.5.Final")
     implementation("org.hsqldb:hsqldb:2.7.2")
     testImplementation("junit:junit:4.13.2")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    providedRuntime("org.springframework.boot:spring-boot-starter-tomcat")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.test {
